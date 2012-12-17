da3201f75352d6780555cab08ecb81f74d665c95
stopiccot <alexey.petruchik@gmail.com>
2012-12-17 15:06:39 +0300

    test FBX opening

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def hello():
-    return flask.render_template('index.html')
+    return fbx_test()#flask.render_template('index.html')
 
 @app.route("/fbx")
 def fbx():
--- fbx_test.py
+++ fbx_test.py
@@ -1,4 +1,35 @@
-from fbx import *
+import fbx
+import FbxCommon
+#from fbx import *
+
+def DisplayHierarchy(pScene):
+    lRootNode = pScene.GetRootNode()
+
+    result = ''
+    for i in range(lRootNode.GetChildCount()):
+        result += DisplayNodeHierarchy(lRootNode.GetChild(i), 0)
+
+    return result
+
+def DisplayNodeHierarchy(pNode, pDepth):
+    result = ""
+    for i in range(pDepth):
+        result += "     "
+
+    result += pNode.GetName()
+    result += '<br>'
+
+    for i in range(pNode.GetChildCount()):
+        result += DisplayNodeHierarchy(pNode.GetChild(i), pDepth + 1)
+
+    return result
 
 def fbx_test():
-    return 'It is a <b>FBX</b> test'
+    lSdkManager, lScene = FbxCommon.InitializeSdkObjects()
+    lResult = FbxCommon.LoadScene(lSdkManager, lScene, './test.fbx')
+    if not lResult:
+        return 'Failed to open FBX scene'
+
+    result = 'FBX scene successfully opened<br>------------------------------------------<br>'
+    result += DisplayHierarchy(lScene)
+    return result
