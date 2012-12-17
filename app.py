6a68bc5c8a103f13987fc106d9bda76a19d6d69d
stopiccot <alexey.petruchik@gmail.com>
2012-12-17 19:23:07 +0300

    very simple file upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import coffeescript, flask
+import coffeescript, flask, werkzeug, os
 from fbx_test import fbx_test
 app = flask.Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'fbx'
 
 @app.route("/")
 def hello():
@@ -10,9 +11,28 @@ def hello():
 def fbx(file):
     return fbx_test()
 
-@app.route('/js/<file>')
+# For serving coffee script code
+@app.route('/coffee/<file>')
 def js(file):
-    return coffeescript.compile_file('views/' + file + '.coffee')
+    return coffeescript.compile_file('coffee/' + file + '.coffee')
+
+@app.route('/upload_fbx', methods = ['GET', 'POST'])
+def upload_file():
+    if flask.request.method == 'POST':
+        file = flask.request.files['file']
+        if file and file.filename.endswith('.fbx'):
+            filename = werkzeug.secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'Fbx was uploaded'
+    return '''
+    <!doctype html>
+    <title>Upload new FBX</title>
+    <h1>Upload new FBX</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__ == "__main__":
     app.run(debug = True)
