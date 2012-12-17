14040c45ec85e0d107d78c07aeba82386268fc9a
stopiccot <alexey.petruchik@gmail.com>
2012-12-17 14:50:52 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import coffeescript, flask
+from fbx_test import fbx_test
+app = flask.Flask(__name__)
+
+@app.route("/")
+def hello():
+    return flask.render_template('index.html')
+
+@app.route("/fbx")
+def fbx():
+    return fbx_test()
+
+@app.route('/js/<file>')
+def js(file):
+    return coffeescript.compile_file('views/' + file + '.coffee')
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', debug = True)
---
+++ fbx_test.py
@@ -0,0 +1,4 @@
+from fbx import *
+
+def fbx_test():
+    return 'It is a <b>FBX</b> test'
